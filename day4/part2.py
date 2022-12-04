44d3ff14a4f599efa9ae42bdebbdbea4a44a8f6b
Ruby Lavender <Ruby.Lavender@softwire.com>
2022-12-04 05:48:43 +0000

    Day 4

---
+++ day4/part2.py
@@ -0,0 +1,6 @@
+data = open('input.txt').read().splitlines()
+
+pairs = [x.split(',') for x in data]
+pairs_split = [[x[0].split('-'), x[1].split('-')] for x in pairs]
+
+print(sum(int(x[0]) <= int(y[0]) <= int(y[1]) <= int(x[1]) or int(y[0]) <= int(x[0]) <= int(y[1]) or int(y[0]) <= int(x[1]) <= int(y[1]) for [x, y] in pairs_split))
